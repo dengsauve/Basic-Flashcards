8a94d151a9a574973abb576c2340f793f5e3b8ef
dengsauve <dengsauve@yahoo.com>
2018-12-19 22:28:17 -0800

    Working on homepage

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php
 
+// load database, configs, etc.
 require 'env.php';
 
-$str = <<<EOF
+// query for all flashcards
+$sql = "select * from flashcards";
+$result = $db->query($sql);
+
+// Create Flashcard Table
+$table = "<table>";
+$table = $table . "<tr><th>Term</th><th>Definition</th></tr>";
+
+// Parse flashcard results into table rows
+if($result->num_rows > 0)
+{
+  while($row = $result->fetch_assoc())
+  {
+    $table = $table . "<tr><td>" . $row['term'] . "</td><td>" . $row['definition'] . "</td></tr>";
+  }
+}
+
+// Close Table
+$table = $table . "</table>";
+
+// HTML Page Header
+$header = <<<EOF
 <!DOCTYPE html>
 <html>
   <head>
@@ -10,10 +32,20 @@ $str = <<<EOF
     <title>title</title>
   </head>
   <body>
-    <h1>Flashcards</h1>
-  </body>
-</html>
 EOF;
+echo $header;
 
-echo $str;
+// Flashcards Section
+echo "<h1>Flashcards</h1>";
+echo $table;
+
+// HTML Footers
+$footer = <<<EOF
+  </body>
+  </html>
+EOF;
+echo $footer;
+
+// close db connections
+require 'end.php';
 ?>
