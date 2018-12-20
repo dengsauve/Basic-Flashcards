488e0bfb274d098bf16f0fb8ab5a6cc9fc824077
dengsauve <dengsauve@yahoo.com>
2018-12-19 20:25:24 -0800

    Initial Setup

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'env.php';
+
+$str = <<<EOF
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+  </head>
+  <body>
+    <h1>Flashcards</h1>
+  </body>
+</html>
+EOF;
+
+echo $str;
+?>
