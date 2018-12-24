2d4f7403135734783486d1a6a2955658744fbb92
dengsauve <dengsauve@yahoo.com>
2018-12-23 18:31:16 -0800

    Refactored

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Starting at the beginning of the card list
 var index = 0;
 
 // Get number of cards
@@ -13,24 +14,26 @@ var total = document.getElementById('total');
 total.innerHTML = numberOfCards + 1;
 position.innerHTML = 1;
 
+// Button Actions
 nextButton.addEventListener('click', function(){
-    if(index < numberOfCards)
-    {
-        var cards = document.getElementsByClassName('flip-card');
-        cards[index].classList.add('hidden');
-        index += 1;
-        position.innerHTML = index + 1;
-        cards[index].classList.remove('hidden');
-    }
+    if(index < numberOfCards) changeCard(1);
 });
 
 previousButton.addEventListener('click', function(){
-    if(index > 0)
-    {
-        var cards = document.getElementsByClassName('flip-card');
-        cards[index].classList.add('hidden');
-        index -= 1;
-        position.innerHTML = index + 1;
-        cards[index].classList.remove('hidden');
-    }
+    if(index > 0) changeCard(-1);
 });
+
+// Logic for switching cards
+function changeCard(incrementor)
+{
+    // Get all flip cards
+    var cards = document.getElementsByClassName('flip-card');
+    // Hide current
+    cards[index].classList.add('hidden');
+    // Get next/previous index
+    index += incrementor;
+    // Update position in sidebar
+    position.innerHTML = index + 1;
+    // Show next/previous
+    cards[index].classList.remove('hidden');
+}
