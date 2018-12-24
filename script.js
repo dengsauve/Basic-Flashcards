f2ee00d769e3df63676a1590e19ed92fe6449358
dengsauve <dengsauve@yahoo.com>
2018-12-23 18:14:47 -0800

    Logic for updating status bar
    Will be refactored soon.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 var index = 0;
+
+// Get number of cards
 var numberOfCards = document.getElementsByClassName('flip-card').length - 1;
+
+// Obtain controls
 var nextButton = document.getElementById('next');
 var previousButton = document.getElementById('previous');
+
+// Obtain position outputs and set
+var position = document.getElementById('position');
+var total = document.getElementById('total');
+total.innerHTML = numberOfCards + 1;
+position.innerHTML = 1;
 
 nextButton.addEventListener('click', function(){
     if(index < numberOfCards)
@@ -9,6 +19,7 @@ nextButton.addEventListener('click', function(){
         var cards = document.getElementsByClassName('flip-card');
         cards[index].classList.add('hidden');
         index += 1;
+        position.innerHTML = index + 1;
         cards[index].classList.remove('hidden');
     }
 });
@@ -19,6 +30,7 @@ previousButton.addEventListener('click', function(){
         var cards = document.getElementsByClassName('flip-card');
         cards[index].classList.add('hidden');
         index -= 1;
+        position.innerHTML = index + 1;
         cards[index].classList.remove('hidden');
     }
 });
